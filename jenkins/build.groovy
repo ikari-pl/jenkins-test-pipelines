446c1b063a9ec8852ff77fa167ab193971700330
dsspielm <daniel.spielman@openx.com>
2019-05-14 15:28:14 -0700

    jenkins/build.groovy

---
+++ jenkins/build.groovy
@@ -0,0 +1,31 @@
+def projectName = "Test Pipelines"
+
+node(label) {
+    try {
+        stage('BUILD') {
+            deleteDir()
+            checkout scm
+            sh 'make clean compile'
+        }
+        
+        stage ('TEST') {
+            sh 'make test'
+        }
+        
+        stage('PACKAGE') {
+            sh 'make package'
+            archiveArtifacts artifacts: '**/target/**/*.jar, **/target/**/*.war', onlyIfSuccessful: true
+            env.PACKAGE_VERSION = readFile 'PACKAGE_VERSION.txt'
+            env.PACKAGE_LOCATION = readFile 'PACKAGE_LOCATION.txt'
+            sh 'echo $PACKAGE_LOCATION'
+        }   
+            if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'develop') {
+                stage('UPLOAD ARTIFACT') {
+                sh 'for pkg in $PACKAGE_LOCATION; do ls $pkg;done'
+                sh 'echo "UPLOADDDDDDDDDDDDD ARTIFAAAAAAAACTS!"'
+                //sh 'for pkg in $PACKAGE_LOCATION; do ox-artifact cp $pkg openx-apps:/7/testing/packages/ ;done'
+                }
+            }
+        }
+}  
+
